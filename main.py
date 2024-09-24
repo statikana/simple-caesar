2b81265bdb47c21d9fb2c0b1529016c13967b3e9
statikana <86629697+statikana@users.noreply.github.com>
2024-09-24 17:19:04 -0400

    cleanup

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-from enum import Enum
 from english_words import get_english_words_set
 
-capital_letters = list(map(chr, range(ord("A"), ord("Z")+1)))
-lowercase = list(map(chr, range(ord("a"), ord("z")+1)))
-numbers = list(map(chr, range(ord("0"), ord("9")+1)))
+uppercase_charset = list(map(chr, range(ord("A"), ord("Z") + 1)))
+lowercase_charset = list(map(chr, range(ord("a"), ord("z") + 1)))
+numeric_charset = list(map(chr, range(ord("0"), ord("9") + 1)))
 
 words = get_english_words_set(["web2"])
 
-# print(words)
 
-def caesar(message: str, *, key: int, keyset: list):
+def caesar(message: str, *, key: int, charset: list):
     words = message.split()
-    return " ".join(caesar_word(word, key=key, keyset=keyset) for word in words)
-    
+    return " ".join(caesar_word(word, key=key, charset=charset) for word in words)
 
-def caesar_word(word: str, *, key: int, keyset: list):
+
+def caesar_word(word: str, *, key: int, charset: list):
     return "".join(
-        keyset[(keyset.index(char) + key) % len(keyset)]
-        if char in keyset
-        else char
+        charset[(charset.index(char) + key) % len(charset)] if char in charset else char
         for char in word
     )
-
 
 
 def n_real_words(message: str, wordset: set[str]):
     return sum(word in wordset for word in message.split())
 
-def find(encrypted: str, *, keyset: list, wordset: set[str]):
+
+def find(encrypted: str, *, charset: list, wordset: set[str]):
     best_matching: int = 0
     best_key: int | None = None
     best_result: str | None = None
-    
-    for i in range(len(keyset)):
-        shifted = caesar(encrypted, key=i, keyset=keyset)
+
+    for i in range(len(charset)):
+        shifted = caesar(encrypted, key=i, charset=charset)
         if (amt := n_real_words(shifted, wordset)) > best_matching:
             best_matching = amt
-            best_result = shifted   
+            best_result = shifted
             best_key = i
     return best_result, best_key
 
 
-capitals_wordset = set(map(str.upper, words))
-message = "AOHA'Z AOL TVZA MVBS, JYBLS, HUK IHK-ALTWLYLK YVKLUA FVB LCLY ZLA FVBY LFLZ VU"
 
-result, key = find(message, keyset=capital_letters, wordset=capitals_wordset)
-print(message, caesar(message, key=key, keyset=capital_letters))
+if __name__ == "__main__":
+    capitals_wordset = set(map(str.upper, words))
+    message = (
+        "AOHA'Z AOL TVZA MVBS, JYBLS, HUK IHK-ALTWLYLK YVKLUA FVB LCLY ZLA FVBY LFLZ VU"
+    )
+    result, key = find(message, charset=uppercase_charset, wordset=capitals_wordset)
+    print(message, caesar(message, key=key, charset=uppercase_charset))
